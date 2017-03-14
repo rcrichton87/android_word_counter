579e11bfccb23e18246c32bef4430957b01a1790
Ross Crichton <rcrichton87@googlemail.com>
2017-03-14 15:59:29 +0000

    app can take a string and provide a count for each word in the string

--- app/src/main/java/com/codeclan/wordcounter/WordCountActivity.java
+++ app/src/main/java/com/codeclan/wordcounter/WordCountActivity.java
@@ -3,6 +3,7 @@ package com.codeclan.wordcounter;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,10 +20,11 @@ public class WordCountActivity extends AppCompatActivity {
         countButton = (Button)findViewById(R.id.count_button);
     }
 
-    public void onCountButtonPressed(){
+    public void onCountButtonPressed(View button){
         String stringToCount = wordCountEditText.getText().toString();
         Intent intent = new Intent(this, ShowWordCountActivity.class);
         intent.putExtra("stringToCount", stringToCount);
         startActivity(intent);
     }
+
 }
